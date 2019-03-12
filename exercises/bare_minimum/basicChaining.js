a233255fd98706614b0ed130db862e6170b7037e
jessehang <hangtimee@gmail.com>
2019-03-12 11:14:48 -0700

    Finished basic promises tests

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,6 +8,9 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
+var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
+var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
+
 var fs = require('fs');
 var Promise = require('bluebird');
 
@@ -15,6 +18,30 @@ var Promise = require('bluebird');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(firstLine) {
+      getGitHubProfileAsync(firstLine) 
+      .then(profile => {
+        profile = JSON.stringify(profile);
+        fs.writeFile(writeFilePath, profile, err=>{
+          if(err) throw err;
+          console.log('File has been saved')
+        })
+      })
+      
+  })
+  
+  // return getGitHubProfileAsync(username)
+  //   .then(existingUser => {
+  //     if (existingUser) {
+  //       return 'User already exists!';
+  //     } else {
+        
+  //       return user;
+  //     }
+  //   })
+    // .then(fs.writeFile(writeFilePath, ))
 };
 
 // Export these functions so we can test them
